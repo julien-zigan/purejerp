198c315938ab37075a5d964c439d2de27f692297
Julien Zigan <zigan.julien@gmail.com>
2024-12-02 22:25:29 +0100

    first commit

---
+++ src/de/papagano/purejerp/Purejerp.java
@@ -0,0 +1,12 @@
+package de.papagano.purejerp
+
+
+public class Purejerp {
+	
+	// entry point to the application
+
+	public static void main(String[] args) {
+		// insert database initilization here
+	}
+
+}
